851074da66129629094a76bc99211233b2e8ac3c
lindeneg <christian@lindeneg.org>
2024-11-13 21:50:18 +0100

    test file

--- main.go
+++ main.go
@@ -2,19 +2,42 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"os/user"
 
+	"github.com/lindeneg/monkey/evaluator"
+	"github.com/lindeneg/monkey/lexer"
+	"github.com/lindeneg/monkey/object"
+	"github.com/lindeneg/monkey/parser"
 	"github.com/lindeneg/monkey/repl"
 )
 
 func main() {
-	user, err := user.Current()
-	if err != nil {
-		panic(err)
+	if len(os.Args) > 1 {
+		data, err := os.ReadFile(os.Args[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+		env := object.NewEnvironment()
+		l := lexer.NewLexer(string(data))
+		p := parser.New(l)
+		program := p.ParseProgram()
+		if len(p.Errors()) > 0 {
+			repl.PrintParserErrors(os.Stdout, p.Errors())
+		}
+		result := evaluator.Eval(program, env)
+		if result != nil && result.Type() == object.ERROR_OBJ {
+			fmt.Println(result.Inspect())
+		}
+	} else {
+		user, err := user.Current()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("Hello %s! This is the Monkey programming language!\n",
+			user.Username)
+		fmt.Printf("Feel free to type in commands\n")
+		repl.Start(os.Stdin, os.Stdout)
 	}
-	fmt.Printf("Hello %s! This is the Monkey programming language!\n",
-		user.Username)
-	fmt.Printf("Feel free to type in commands\n")
-	repl.Start(os.Stdin, os.Stdout)
 }
---
+++ main.monkey
@@ -0,0 +1,20 @@
+let first = "Foo";
+let last = "Bar";
+
+let make_name = fn(first, last) {
+    return first + " " + last;
+};
+
+let first_ = "Christian";
+let last_ = "Lindeneg";
+
+let name = make_name(first_, last_)
+
+println(name);
+
+let x = 10;
+let y = 20;
+let c = x + y;
+
+println(c);
+
