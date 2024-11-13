c876cbe11fc8fd5fbbdba86de559f8038cbca5ec
lindeneg <christian@lindeneg.org>
2024-11-13 21:50:02 +0100

    simple println function

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,9 +1,13 @@
 package evaluator
 
-import "github.com/lindeneg/monkey/object"
+import (
+	"fmt"
+
+	"github.com/lindeneg/monkey/object"
+)
 
 var builtins = map[string]*object.Builtin{
-	"len": &object.Builtin{
+	"len": {
 		Fn: func(args ...object.Object) object.Object {
 			if len(args) != 1 {
 				return newError("wrong number of arguments. got=%d, want=1",
@@ -12,6 +16,25 @@ var builtins = map[string]*object.Builtin{
 			switch arg := args[0].(type) {
 			case *object.String:
 				return &object.Integer{Value: int64(len(arg.Value))}
+			default:
+				return newError("argument to `len` not supported, got %s",
+					args[0].Type())
+			}
+		},
+	},
+	"println": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1",
+					len(args))
+			}
+			switch arg := args[0].(type) {
+			case *object.String:
+				fmt.Println(arg.Value)
+				return nil
+			case *object.Integer:
+				fmt.Printf("%d\n", arg.Value)
+				return nil
 			default:
 				return newError("argument to `len` not supported, got %s",
 					args[0].Type())
